ae03ef3790d2ff8384f0a3ace617600b20f3f7cb
gaharuty-stack <gaharuty@gmail.com>
2026-07-20 02:17:40 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 set -e
 
-# Бот слушает на порту 5000
 python bot.py &
 
 sleep 3
 
-# Tollbooth проксирует с порта 10000 (внешний) на 5000 (внутренний)
+PORT=${PORT:-10000}
 agent402-tollbooth \
   --target http://localhost:5000 \
   --config tollbooth.config.json \
-  --port 10000 \
+  --port $PORT \
   --host 0.0.0.0
 
 wait
